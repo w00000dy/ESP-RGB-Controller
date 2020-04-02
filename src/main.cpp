05992ca81dfc3361f544cafb5bdce9a180b581a2
Woody <>
2020-04-02 12:53:56 +0200

    full working web

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,9 @@
 #define CLOCK_PIN 14
 
 int x = 1;
-String indexHTMLString;
-String materializeCSSString;
-String styleCSSString;
-String materializeJSString;
-String syncerJSString;
+bool lightActive = false;
+bool rainbowActive = false;
+String response;
 reaction rainbow;
 reaction ota;
 
@@ -70,43 +68,97 @@ start()
     Serial.print("IP Addresse: ");
     Serial.println(WiFi.localIP());
 
-    SPIFFS.begin();                                                     // mount filesystem
+    SPIFFS.begin(); // mount filesystem
 
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {                    
-       request->send(SPIFFS, "/index.html", "text/html");
-        
+    // main web page
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+        request->send(SPIFFS, "/index.html", "text/html");
     });
 
     server.on("/css/materialize.min.css", HTTP_GET, [](AsyncWebServerRequest* request) {
         request->send(SPIFFS, "/css/materialize.min.css", "text/css");
-       
     });
 
     server.on("/css/style.css", HTTP_GET, [](AsyncWebServerRequest* request) {
         request->send(SPIFFS, "/css/style.css", "text/css");
-       
     });
 
     server.on("/js/materialize.min.js", HTTP_GET, [](AsyncWebServerRequest* request) {
         request->send(SPIFFS, "/js/materialize.min.js", "text/javascript");
-       
     });
 
     server.on("/js/syncer.js", HTTP_GET, [](AsyncWebServerRequest* request) {
         request->send(SPIFFS, "/js/syncer.js", "text/javascript");
-       
     });
 
-    server.on("/on", HTTP_GET, [](AsyncWebServerRequest* request) {
-        rainbow = app.repeat(10, rainbowLED);
-        request->send(200, "text/html", "an");
+    // web controller for rgb strip
+    server.on("/effect", HTTP_POST, [](AsyncWebServerRequest* request) {
+        if (request->hasParam("light", true)) { // light
+            response = request->getParam("light", true)->value();
+            FastLED.show();
+            if (response == "true") {
+                // Disable old effect
+                rainbowActive = false;
+                app.disable(rainbow);
+                // Enable new effect
+                lightActive = true;
+                fill_solid(leds, NUM_LEDS, CRGB::White);
+                FastLED.show();
+                request->send(200, "text/plain", response);
+            } else if (response = "false") {
+                // Disable effect
+                lightActive = false;
+                fill_solid(leds, NUM_LEDS, CRGB::Black);
+                FastLED.show();
+                request->send(200, "text/plain", response);
+            } else {
+                request->send(200, "text/plain", "Error! Invalid parameter. (light)");
+            }
+        } else if (request->hasParam("rainbow", true)) { // rainbow
+            response = request->getParam("rainbow", true)->value();
+            if (response == "true") {
+                // Disable old effect
+                lightActive = false;
+                // Enable new effect
+                rainbowActive = true;
+                rainbow = app.repeat(10, rainbowLED);
+                request->send(200, "text/plain", response);
+            } else if (response = "false") {
+                // Disable effect
+                rainbowActive = false;
+                app.disable(rainbow);
+                fill_solid(leds, NUM_LEDS, CRGB::Black);
+                FastLED.show();
+                request->send(200, "text/plain", response);
+            } else {
+                request->send(200, "text/plain", "Error! Invalid parameter. (rainbow)");
+            }
+        } else {
+            request->send(200, "text/plain", "Error! No parameters found.");
+        }
     });
 
-    server.on("/off", HTTP_GET, [](AsyncWebServerRequest* request) {
-        app.disable(rainbow);
-        fill_solid(leds, NUM_LEDS, CRGB::Black);
-        FastLED.show();
-        request->send(200, "text/html", "aus");
+    server.on("/sync", HTTP_POST, [](AsyncWebServerRequest* request) {
+        if (request->hasParam("effect", true)) { // light
+            response = request->getParam("effect", true)->value();
+            if (response == "light") {
+                if (lightActive == true) {
+                    request->send(200, "text/plain", "true");
+                } else {
+                    request->send(200, "text/plain", "false");
+                }
+            } else if (response == "rainbow") {
+                if (rainbowActive == true) {
+                    request->send(200, "text/plain", "true");
+                } else {
+                    request->send(200, "text/plain", "false");
+                }
+            } else {
+                request->send(200, "text/plain", "Error! Invalid parameter. (effect)");
+            }
+        } else {
+            request->send(200, "text/plain", "Error! No parameters found.");
+        }
     });
 
     server.onNotFound(notFound);
