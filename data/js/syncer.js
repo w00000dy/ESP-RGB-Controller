de7a187a0d28fb31b30173415671cba21027cd91
Woody <>
2020-04-02 20:12:52 +0200

    new animation

--- data/js/syncer.js
+++ data/js/syncer.js
@@ -1,15 +1,24 @@
 document.getElementById("light").addEventListener("click", light);
 document.getElementById("rainbow").addEventListener("click", rainbow);
+document.getElementById("fire").addEventListener("click", fire);
 document.getElementById("weAreNumberOne").addEventListener("click", weAreNumberOne);
 
 function light() {
     document.getElementById('rainbow').checked = false;
+    document.getElementById('fire').checked = false;
     sendData("light");
 }
 
 function rainbow() {
     document.getElementById('light').checked = false;
+    document.getElementById('fire').checked = false;
     sendData("rainbow");
+}
+
+function fire() {
+    document.getElementById('light').checked = false;
+    document.getElementById('rainbow').checked = false;
+    sendData("fire");
 }
 
 function weAreNumberOne() {
@@ -76,10 +85,29 @@ function syncRainbow() {
             else {
                 document.getElementById('rainbow').checked = false;
             }
-            syncWeAreNumberOne();
+            syncFire();
         }
     }
     xhr.send("effect=rainbow");
+}
+
+function syncFire() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/sync', true);
+    //Send the proper header information along with the request
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () { // Call a function when the state changes.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            if (xhr.response == "true") {
+                document.getElementById('fire').checked = true;
+            }
+            else {
+                document.getElementById('fire').checked = false;
+            }
+            syncWeAreNumberOne();
+        }
+    }
+    xhr.send("effect=fire");
 }
 
 function syncWeAreNumberOne() {
--- src/main.cpp
+++ src/main.cpp
@@ -49,9 +49,11 @@ int i = 0;
 uint64 nextPlay = 0;
 bool lightActive = false;
 bool rainbowActive = false;
+bool fireActive = false;
 bool weAreNumberOneActive = false;
 String response;
 reaction rainbow;
+reaction fire;
 reaction music;
 
 CRGB leds[NUM_LEDS];
@@ -60,8 +62,51 @@ AsyncWebServer server(80);
 const char* ssid = "";
 const char* password = "";
 
-void notFound(AsyncWebServerRequest* request) {
-    request->send(404, "text/html", "Nicht gefunden!");
+// COOLING: How much does the air cool as it rises?
+// Less cooling = taller flames.  More cooling = shorter flames.
+// Default 50, suggested range 20-100
+#define COOLING 65
+
+// SPARKING: What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire.  Lower chance = more flickery fire.
+// Default 120, suggested range 50-200.
+#define SPARKING 120
+
+// reverse the direction of the fire
+bool gReverseDirection = true;
+
+void fireLED() {
+    // Array of temperature readings at each simulation cell
+    static byte heat[NUM_LEDS];
+
+    // Step 1.  Cool down every cell a little
+    for (int i = 0; i < NUM_LEDS; i++) {
+        heat[i] = qsub8(heat[i], random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    }
+
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for (int k = NUM_LEDS - 1; k >= 2; k--) {
+        heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
+    }
+
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if (random8() < SPARKING) {
+        int y = random8(7);
+        heat[y] = qadd8(heat[y], random8(160, 255));
+    }
+
+    // Step 4.  Map from heat cells to LED colors
+    for (int j = 0; j < NUM_LEDS; j++) {
+        CRGB color = HeatColor(heat[j]);
+        int pixelnumber;
+        if (gReverseDirection) {
+            pixelnumber = (NUM_LEDS - 1) - j;
+        } else {
+            pixelnumber = j;
+        }
+        leds[pixelnumber] = color;
+    }
+    FastLED.show();
 }
 
 void rainbowLED() {
@@ -87,6 +132,10 @@ void playWeAreNumberOne() {
             i = 0;
         }
     }
+}
+
+void notFound(AsyncWebServerRequest* request) {
+    request->send(404, "text/html", "Nicht gefunden!");
 }
 
 void start() {
@@ -136,6 +185,8 @@ void start() {
                 // Disable old effect
                 rainbowActive = false;
                 app.disable(rainbow);
+                fireActive = false;
+                app.disable(fire);
                 // Enable new effect
                 lightActive = true;
                 fill_solid(leds, NUM_LEDS, CRGB::White);
@@ -155,6 +206,8 @@ void start() {
             if (response == "true") {
                 // Disable old effect
                 lightActive = false;
+                fireActive = false;
+                app.disable(fire);
                 // Enable new effect
                 rainbowActive = true;
                 rainbow = app.repeat(10, rainbowLED);
@@ -163,6 +216,27 @@ void start() {
                 // Disable effect
                 rainbowActive = false;
                 app.disable(rainbow);
+                fill_solid(leds, NUM_LEDS, CRGB::Black);
+                FastLED.show();
+                request->send(200, "text/plain", response);
+            } else {
+                request->send(200, "text/plain", "Error! Invalid parameter. (rainbow)");
+            }
+        } else if (request->hasParam("fire", true)) {  // fire
+            response = request->getParam("fire", true)->value();
+            if (response == "true") {
+                // Disable old effect
+                lightActive = false;
+                rainbowActive = false;
+                app.disable(rainbow);
+                // Enable new effect
+                fireActive = true;
+                fire = app.repeat(30, fireLED);
+                request->send(200, "text/plain", response);
+            } else if (response = "false") {
+                // Disable effect
+                fireActive = false;
+                app.disable(fire);
                 fill_solid(leds, NUM_LEDS, CRGB::Black);
                 FastLED.show();
                 request->send(200, "text/plain", response);
@@ -209,6 +283,12 @@ void start() {
                 }
             } else if (response == "rainbow") {
                 if (rainbowActive == true) {
+                    request->send(200, "text/plain", "true");
+                } else {
+                    request->send(200, "text/plain", "false");
+                }
+            } else if (response == "fire") {
+                if (fireActive == true) {
                     request->send(200, "text/plain", "true");
                 } else {
                     request->send(200, "text/plain", "false");
